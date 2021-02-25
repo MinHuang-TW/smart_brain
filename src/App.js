8ee61f6608d5c6c66c2f741ed9aa8811790545d7
Min Huang <min@mavisoft.com>
2021-02-25 09:27:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Clarifai from 'clarifai';
+import Particles from 'react-particles-js';
+import Signin from './components/Signin';
+import Register from './components/Register';
+import FaceRecognition from './components/FaceRecognition';
+import Navigation from './components/Navigation';
+import Logo from './components/Logo';
+import Rank from './components/Rank';
+import ImageLinkForm from './components/ImageLinkForm';
 import './App.css';
 
-function App() {
+const app = new Clarifai.App({
+  apiKey: '65e9f3c02c284459abfdd25ee662cf5b',
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState('signin');
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  };
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setImageUrl(input);
+
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, input)
+      .then((response) => displayFaceBox(calculateFaceLocation(response)))
+      .catch((error) => console.log(error));
+  };
+
+  const handleRouteChange = (route) => {
+    if (route === 'signout') {
+      setIsSignedIn(false);
+    } else if (route === 'home') {
+      setIsSignedIn(true);
+    }
+    setRoute(route);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Particles className='particles' params={particlesOptions} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={handleRouteChange} />
+      {route === 'home' ? (
+        <>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={handleChange}
+            onButtonSubmit={handleSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+        </>
+      ) : route === 'signin' ? (
+        <Signin onRouteChange={handleRouteChange} />
+      ) : (
+        <Register onRouteChange={handleRouteChange} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ImageLinkForm/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => (
+  <div className=''>
+    <p className='f3'>
+      This Magic Brain will detect faces in your pictures. Give it a try.
+    </p>
+    <div className='center'>
+      <div className='center form pa4 br3 shadow-5'>
+        <input
+          className='f4 pa2 w-70 center'
+          type='text'
+          onChange={onInputChange}
+        />
+        <button
+          className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+          onClick={onButtonSubmit}
+        >
+          Detect
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default ImageLinkForm;
